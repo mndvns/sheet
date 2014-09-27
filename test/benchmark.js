2655bc8cf1fb636ad308dd833e10c300c44f9f36
Michael Vanasse <mail@mndvns.com>
2014-09-26 23:57:33 -0600

    improve benchmarks, flesh out rule interface

--- test/benchmark.js
+++ test/benchmark.js
@@ -1,13 +1,31 @@
 var Sheet = window.Sheet = (require('sheet') || window.Sheet);
-var sheet = window.sheet = new Sheet('testing');
+var sheet = window.sheet = new Sheet();
 
-bench(1000, {per: .5}, function(){
-  sheet.addRule('a.test', 'color: red');
-});
+group(100);
+group(1000);
+group(2000);
 
-bench(1000, {per: .5}, function(){
-  sheet.addRule('.test-container *', 'color: blue');
-});
+function group(q, opts){
+  create('h1', q);
+
+  bench(q, opts, function(){
+    sheet.insertRule('label.test', 'color: red');
+  });
+
+  bench(q, opts, function(){
+    sheet.addRule('span.test { color: red; background: yellow }');
+  });
+
+  bench(q, opts, function(i){
+    sheet.insertRule('div.test' + i, 'color: red');
+  });
+
+  bench(q, opts, function(i){
+    sheet.addRule('input.test' + i, 'color: red');
+  });
+
+  create('hr');
+}
 
 function bench(ceiling, limit, cb){
   var start = Date.now();
@@ -17,14 +35,13 @@ function bench(ceiling, limit, cb){
     if (i === ceiling) end = Date.now();
   }
 
-
   var name = cb.toString().split('\n').slice(1, -1).join('\n');
   create('ul', name);
 
   var result = {};
   result.warn = false;
   result.total = end - start;
-  result.per = Math.round((result.total / ceiling) * 10000) / 10000;
+  result.per = Math.round((result.total / ceiling) * 1000)
   for (var k in limit) {
     if (!limit.hasOwnProperty(k)) continue;
     if (result.warn === true) break;
@@ -32,7 +49,7 @@ function bench(ceiling, limit, cb){
   }
 
   create('li', result.total + ' ms', result.warn);
-  create('li', result.per + ' ms', result.warn);
+  create('li', result.per + ' µs', result.warn);
 }
 
 function create(tag, msg, warn){
--- test/tests.js
+++ test/tests.js
@@ -1,53 +1,105 @@
 var Sheet = window.Sheet = (require('sheet') || window.Sheet);
-var sheet = window.sheet = new Sheet('testing');
+var sheet = window.sheet = new Sheet();
+var rule = window.rule = sheet.addRule('.test-container .test', 'color: orange');
 var $ = window.jQuery;
 
-describe('Sheet', function(){
-
-  it('should add a rule to the page', function(){
-    sheet.addRule('span.test', 'color: blue');
-    $('span.test').css('color').should.startWith('rgb(0, 0, 255)');
+describe('Rule', function(){
+  describe('#remove', function(){
+    it('should remove itself', function(){
+      $(rule.selector).css('color').should.startWith('rgb(255, 165, 0');
+      rule.remove();
+      $(rule.selector).css('color').should.startWith('rgb(0, 0, 0');
+    });
   });
 
-  it('should remove a rule by index', function(){
-    sheet.removeRule(0);
-    $('span.test').css('color').should.startWith('rgb(0, 0, 0)');
+  describe('#add', function(){
+    it('should add itself', function(){
+      rule.add();
+      $(rule.selector).css('color').should.startWith('rgb(255, 165, 0');
+    });
   });
 
-  it('should remove a rule by selector', function(){
-    sheet.addRule('label.test', 'color: blue');
-    sheet.removeRule('label.test');
-    $('label.test').css('color').should.startWith('rgb(0, 0, 0');
+  describe('#query', function(){
+    it('should find single element', function(){
+      $(rule.selector)[0].should.eql(rule.query());
+    });
   });
 
-  it('should let rules remove themselves', function(){
-    sheet.addRule('label.test', 'color: orange').remove();
-    $('label.test').css('color').should.startWith('rgb(0, 0, 0');
+  describe('#queryAll', function(){
+    it('should find all elements', function(){
+      var queries = rule.queryAll();
+      for (var i = 0, l = queries.length; i < l; i++) {
+        $(rule.selector)[i].should.eql(queries[i]);
+      }
+    });
   });
 
-  it('should lookup a rule exactly by string', function(){
-    sheet.addRule('span.test', 'color: green');
-    var lu = sheet.lookup('span.test');
-    lu.selector.should.equal('span.test');
-    lu.property.should.equal('color: green');
-    lu.remove();
-  });
-
-  it('should merge properties of a common selector', function(){
-    sheet.addRule('a.test', 'color: green');
-    sheet.addRule('a.test', 'font-size: 12px');
-    ([sheet.length()]).should.eql([1]);
-    sheet.cssRules[0].cssText.should.equal('a.test { color: green; font-size: 12px; }');
-    sheet.lookup('a.test').remove();
-  });
-
-  it('should remove itself', function(){
-    var head = document.getElementsByTagName('head')[0];
-    var pre = head.children.length;
+  after(function(){
     sheet.remove();
-    var post = head.children.length;
-    // wrapped in array because of ie bug in should.js
-    ([pre]).should.eql([post + 1]);
+    sheet = new Sheet();
+  });
+});
+
+describe('Sheet', function(){
+  describe('#addRule', function(){
+    it('should accept split format', function(){
+      sheet.addRule('span.test', 'color: blue');
+      $('span.test').css('color').should.startWith('rgb(0, 0, 255)');
+    });
+
+    it('should accept flat format', function(){
+      sheet.addRule('span.test { color: red }');
+      $('span.test').css('color').should.startWith('rgb(255, 0, 0)');
+    });
   });
 
+  describe('#removeRule', function(){
+    it('should remove a rule by index', function(){
+      sheet.removeRule(1);
+      sheet.removeRule(0);
+      $('span.test').css('color').should.startWith('rgb(0, 0, 0)');
+    });
+
+    it('should remove a rule by selector', function(){
+      sheet.addRule('label.test', 'color: blue');
+      sheet.removeRule('label.test');
+      $('label.test').css('color').should.startWith('rgb(0, 0, 0');
+    });
+  });
+
+  describe('#lookup', function(){
+    it('should lookup a rule exactly by string', function(){
+      sheet.addRule('span.test', 'color: green');
+      var lu = sheet.lookup('span.test');
+      lu.selector.should.equal('span.test');
+      lu.property.should.equal('color: green');
+      lu.remove();
+    });
+  });
+
+  describe('#insertRule', function(){
+    it('should merge properties of a common selector', function(){
+      sheet.insertRule('a.test', 'color: green');
+      sheet.insertRule('a.test', 'font-size: 12px');
+      ([sheet.length()]).should.eql([1]);
+      sheet.cssRules[0].cssText.should.equal('a.test { color: green; font-size: 12px; }');
+      sheet.lookup('a.test').remove();
+    });
+  });
+
+  describe('#remove', function(){
+    it('should remove itself', function(){
+      var head = document.getElementsByTagName('head')[0];
+      var pre = head.children.length;
+      sheet.remove();
+      var post = head.children.length;
+      // wrapped in array because of ie bug in should.js
+      ([pre]).should.eql([post + 1]);
+    });
+  });
+
+  after(function(){
+    // sheet.remove();
+    sheet = new Sheet();
+  });
 });
